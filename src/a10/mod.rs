aabb7e624947b06f55f5d023aae5e8982b7f0760
Jan Kaczmarkiewicz <positiveKiddo@gmail.com>
2021-12-22 21:42:23 +0100

    feat: solution to 10 problem (second part)

--- src/a10/mod.rs
+++ src/a10/mod.rs
@@ -1,50 +1,90 @@
+use itertools::Itertools;
 use crate::utils::read_lines;
 
-fn a() -> u32 {
-    read_lines("src/a10/input").map(|line| {
-        let chars = line
-            .chars()
-            .collect::<Vec<_>>();
+fn get_line_error_score(line: &String) -> u32 {
+    let chars = line
+        .chars()
+        .collect::<Vec<_>>();
 
-        let mut opened = vec![];
+    let mut error_score = 0;
+    let mut opened = vec![];
 
-        let mut error_score = 0;
-
-        for char in chars {
-            match char {
-                '{' | '<' | '(' | '[' => {
-                    opened.push(char)
-                }
-                ']' => {
-                    if opened.pop().unwrap() != '[' {
-                        error_score += 57;
-                    }
-                }
-                '}' => {
-                    if opened.pop().unwrap() != '{' {
-                        error_score += 1197;
-                    }
-                }
-                '>' => {
-                    if opened.pop().unwrap() != '<' {
-                        error_score += 25137;
-                    }
-                }
-                ')' => {
-                    if opened.pop().unwrap() != '(' {
-                        error_score += 3;
-                    }
-                }
-                _ => {}
+    for char in chars {
+        match char {
+            '{' | '<' | '(' | '[' => {
+                opened.push(char)
             }
+            ']' => {
+                if opened.pop().unwrap() != '[' {
+                    error_score += 57;
+                }
+            }
+            '}' => {
+                if opened.pop().unwrap() != '{' {
+                    error_score += 1197;
+                }
+            }
+            '>' => {
+                if opened.pop().unwrap() != '<' {
+                    error_score += 25137;
+                }
+            }
+            ')' => {
+                if opened.pop().unwrap() != '(' {
+                    error_score += 3;
+                }
+            }
+            _ => {}
         }
-
-        error_score
-    }).fold(0, |prev, curr| curr + prev)
+    }
+    error_score
 }
 
+fn a() -> u32 {
+    read_lines("src/a10/input")
+        .map(|line| get_line_error_score(&line))
+        .fold(0, |prev, curr| curr + prev)
+}
+
+fn b() -> u64 {
+    let sorted_result = read_lines("src/a10/input")
+        .filter(|line| get_line_error_score(line) == 0)
+        .map(|line| {
+            let chars = line
+                .chars()
+                .collect::<Vec<_>>();
+
+            let mut opened = vec![];
+
+            for char in chars {
+                match char {
+                    '{' | '<' | '(' | '[' => { opened.push(char); },
+                    ']' | '}' | '>' | ')' => { opened.pop(); },
+                    _ => {}
+                }
+            }
+
+            opened.iter().rev().fold(0, |prev, curr| {
+                let modifier: u64 = match curr {
+                    '(' => 1,
+                    '[' => 2,
+                    '{' => 3,
+                    '<' => 4,
+                    _ => 0,
+                };
+                5 * prev + modifier
+            })
+    }).sorted().collect::<Vec<_>>();
+
+    sorted_result[sorted_result.len() / 2]
+}
+
+
+
+
+
 pub fn answer() {
-    println!("Answer to problem 10: {}", a());
+    println!("Answer to problem 10: {} {}", a(), b());
 }
 
 
