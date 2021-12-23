45946861c8159cbd129ad4d020bdba7d207dd887
Jan Kaczmarkiewicz <positiveKiddo@gmail.com>
2021-12-23 21:24:54 +0100

    feat: solution to 11 problem (second part)

--- src/a11/mod.rs
+++ src/a11/mod.rs
@@ -1,7 +1,7 @@
 use crate::utils::read_lines;
 
-fn a() -> u32 {
-    let mut octo = read_lines("src/a11/input")
+fn read_octopuses() -> Vec<Vec<u32>> {
+     read_lines("src/a11/input")
         .map(|line|
             line
                 .chars()
@@ -9,60 +9,89 @@ fn a() -> u32 {
                     char.to_digit(10).unwrap()
                 })
                 .collect::<Vec<_>>())
-        .collect::<Vec<_>>();
+        .collect::<Vec<_>>()
+}
 
-    fn flash_rec(x: usize, y: usize, octo: &mut Vec<Vec<u32>>) {
+fn flash_rec(x: usize, y: usize, octopuses: &mut Vec<Vec<u32>>) {
 
-        if octo[y][x] > 9 {
-            octo[y][x] = 0;
+    if octopuses[y][x] > 9 {
+        octopuses[y][x] = 0;
 
-            let x = x as i32;
-            let y = y as i32;
+        let x = x as i32;
+        let y = y as i32;
 
-            let directions = vec![
-                (x, y - 1),
-                (x, y + 1),
-                (x + 1, y),
-                (x - 1, y),
-                (x - 1, y - 1),
-                (x + 1, y + 1),
-                (x - 1, y + 1),
-                (x + 1, y - 1),
-            ];
+        let directions = vec![
+            (x, y - 1),
+            (x, y + 1),
+            (x + 1, y),
+            (x - 1, y),
+            (x - 1, y - 1),
+            (x + 1, y + 1),
+            (x - 1, y + 1),
+            (x + 1, y - 1),
+        ];
 
-            for (x, y) in directions {
-                if x >= 0 && y >= 0 && x < octo[0].len() as i32  && y < octo.len() as i32 && octo[y as usize][x as usize] != 0 {
-                    octo[y as usize ][x as usize] += 1;
-                    flash_rec(x as usize, y as usize, octo);
-                }
+        for (x, y) in directions {
+            if x >= 0 && y >= 0 && x < octopuses[0].len() as i32  && y < octopuses.len() as i32 && octopuses[y as usize][x as usize] != 0 {
+                octopuses[y as usize ][x as usize] += 1;
+                flash_rec(x as usize, y as usize, octopuses);
             }
         }
     }
+}
+
+fn a() -> u32 {
+    let mut octopuses = read_octopuses();
 
     let mut sum = 0;
 
     for _i in 0..100 {
-        for y in 0..octo.len() {
-            for x in 0..octo[y].len() {
-                octo[y][x] += 1;
+        for y in 0..octopuses.len() {
+            for x in 0..octopuses[y].len() {
+                octopuses[y][x] += 1;
             }
         }
 
-        for y in 0..octo.len() {
-            for x in 0..octo[y].len() {
-                flash_rec(x, y, &mut octo);
+        for y in 0..octopuses.len() {
+            for x in 0..octopuses[y].len() {
+                flash_rec(x, y, &mut octopuses);
             }
         }
 
-        sum += octo.iter().flatten().fold(0, |prev, curr| if *curr == 0 {1} else {0} + prev )
+        sum += octopuses.iter().flatten().fold(0, |prev, curr| if *curr == 0 {1} else {0} + prev )
     }
 
     sum
 }
 
+fn b() -> u32 {
+    let mut octopuses = read_octopuses();
+
+    for i in 0..1000 {
+        for y in 0..octopuses.len() {
+            for x in 0..octopuses[y].len() {
+                octopuses[y][x] += 1;
+            }
+        }
+
+        for y in 0..octopuses.len() {
+            for x in 0..octopuses[y].len() {
+                flash_rec(x, y, &mut octopuses);
+            }
+        }
+
+        if octopuses.iter().flatten().all(|x| *x == 0) {
+            return i + 1;
+        }
+    }
+
+    0
+}
+
+
 
 pub fn answer() {
-    println!("Answer to problem 11: {}", a());
+    println!("Answer to problem 11: {}, {}", a(), b());
 }
 
 
@@ -73,5 +102,10 @@ mod tests {
     #[test]
     fn should_solve_first_problem() {
         assert_eq!(a(), 1713);
+    }
+
+    #[test]
+    fn should_solve_second_problem() {
+        assert_eq!(a(), 502);
     }
 }
