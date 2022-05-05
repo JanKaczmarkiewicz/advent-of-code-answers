7addaf4d0ed583e5c578350e2f59505f5df3a7ee
JanKaczmarkiewicz <positiveKiddo@gmail.com>
2022-05-05 23:23:43 +0200

    refactor(17): split logic into functions

--- src/a17/mod.rs
+++ src/a17/mod.rs
@@ -1,6 +1,13 @@
 use crate::utils::read;
 
-fn a() -> i32 {
+struct Config {
+    start_x: i32,
+    end_x: i32,
+    start_y: i32,
+    end_y: i32,
+}
+
+fn get_config() -> Config {
     let input_raw = read("src/a17/input");
 
     let result = str::replace(&input_raw, "target area: x=", "");
@@ -16,13 +23,22 @@ fn a() -> i32 {
     let start_y = coordinates.next().unwrap();
     let end_y = coordinates.next().unwrap();
 
-    let possible_x = (1..=end_x)
+    Config {
+        start_x,
+        end_x,
+        start_y,
+        end_y,
+    }
+}
+
+fn get_possible_x(config: &Config) -> Vec<i32> {
+    (1..=config.end_x)
         .filter(|x| {
             let mut current = 0;
             let mut velocity = *x;
 
-            while velocity != 0 && current <= end_x {
-                if (start_x..=end_x).contains(&current) {
+            while velocity != 0 && current <= config.end_x {
+                if (config.start_x..=config.end_x).contains(&current) {
                     return true;
                 }
                 current += velocity;
@@ -30,15 +46,17 @@ fn a() -> i32 {
             }
             return false;
         })
-        .collect::<Vec<_>>();
+        .collect::<Vec<_>>()
+}
 
-    let mut possible_y = (0..(-1 * start_y))
+fn get_possible_y(config: &Config) -> Vec<i32> {
+    (0..(-1 * config.start_y))
         .filter(|y| {
             let mut current = 0;
             let mut velocity = *y;
 
-            while current >= start_y {
-                if (start_y..=end_y).contains(&current) {
+            while current >= config.start_y {
+                if (config.start_y..=config.end_y).contains(&current) {
                     return true;
                 }
                 current += velocity;
@@ -46,22 +64,29 @@ fn a() -> i32 {
             }
             return false;
         })
-        .rev();
+        .rev()
+        .collect::<Vec<_>>()
+}
 
-    let highest_y = possible_y
+fn get_highest_y(config: &Config) -> i32 {
+    let possible_x = get_possible_x(config);
+    let possible_y = get_possible_y(config);
+
+    possible_y
+        .iter()
         .find_map(|y| {
             for x in &possible_x {
                 let mut current_x = 0;
                 let mut velocity_x = *x;
 
                 let mut current_y = 0;
-                let mut velocity_y = y;
+                let mut velocity_y = *y;
 
                 let mut highest_y = current_y;
 
-                while current_y >= start_y && current_x <= end_x {
-                    if (start_y..=end_y).contains(&current_y)
-                        && (start_x..=end_x).contains(&current_x)
+                while current_y >= config.start_y && current_x <= config.end_x {
+                    if (config.start_y..=config.end_y).contains(&current_y)
+                        && (config.start_x..=config.end_x).contains(&current_x)
                     {
                         return Some(highest_y);
                     }
@@ -80,9 +105,13 @@ fn a() -> i32 {
             }
             return None;
         })
-        .unwrap();
+        .unwrap()
+}
 
-    highest_y
+fn a() -> i32 {
+    let config = get_config();
+
+    get_highest_y(&config)
 }
 
 pub fn answer() {
