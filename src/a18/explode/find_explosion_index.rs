280964309bfd90b3b878b898c4fd3bbf505c373c
JanKaczmarkiewicz <jan.michal.kaczmarkiewicz@gmail.com>
2022-08-21 08:47:03 +0200

    feat(18): explode without changing to 0

--- src/a18/explode/find_explosion_index.rs
+++ src/a18/explode/find_explosion_index.rs
@@ -1,10 +1,10 @@
 use crate::a18::data::Data;
 
-fn find_explosion_index_inner(d: &mut Data, depth: usize, index: &mut i32) -> bool {
+fn find_explosion_index_inner(d: &Data, depth: usize, index: &mut i32) -> bool {
     match d {
         Data::Pair(pair) => {
-            let left = &mut pair.0;
-            let right = &mut pair.1;
+            let left = &pair.0;
+            let right = &pair.1;
 
             if let (Data::Integer(_), Data::Integer(_)) = (&left, &right) {
                 if depth >= 4 {
@@ -27,10 +27,10 @@ fn find_explosion_index_inner(d: &mut Data, depth: usize, index: &mut i32) -> bo
     return false;
 }
 
-pub fn find_explosion_index(mut d: Data) -> Option<usize> {
+pub fn find_explosion_index(d: &Data) -> Option<usize> {
     let mut index = -1;
 
-    if find_explosion_index_inner(&mut d, 0, &mut index) {
+    if find_explosion_index_inner(&d, 0, &mut index) {
         Some((index + 1) as usize)
     } else {
         None
@@ -41,29 +41,43 @@ pub fn find_explosion_index(mut d: Data) -> Option<usize> {
 mod tests {
     use pretty_assertions::assert_eq;
 
+    use crate::a18::data::Data;
+
     use super::find_explosion_index;
 
-    fn setup(raw: &str) -> Option<usize> {
-        find_explosion_index(serde_json::from_str(raw).unwrap())
+    fn parse(raw: &str) -> Data {
+        serde_json::from_str(raw).unwrap()
     }
 
     #[test]
     fn find_explosion_index_one() {
-        assert_eq!(setup("[[[[[9,8],1],2],3],4]"), Some(0));
+        assert_eq!(
+            find_explosion_index(&parse("[[[[[9,8],1],2],3],4]")),
+            Some(0)
+        );
     }
 
     #[test]
     fn find_explosion_index_two() {
-        assert_eq!(setup("[7,[6,[5,[4,[3,2]]]]]"), Some(4));
+        assert_eq!(
+            find_explosion_index(&parse("[7,[6,[5,[4,[3,2]]]]]")),
+            Some(4)
+        );
     }
 
     #[test]
     fn find_explosion_index_three() {
-        assert_eq!(setup("[[6,[5,[4,[3,2]]]],1]"), Some(3));
+        assert_eq!(
+            find_explosion_index(&parse("[[6,[5,[4,[3,2]]]],1]")),
+            Some(3)
+        );
     }
 
     #[test]
     fn find_explosion_index_four() {
-        assert_eq!(setup("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]"), Some(7));
+        assert_eq!(
+            find_explosion_index(&parse("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]")),
+            Some(7)
+        );
     }
 }
--- src/a18/explode/mod.rs
+++ src/a18/explode/mod.rs
@@ -4,10 +4,22 @@ use super::data::Data;
 use find_explosion_index::find_explosion_index;
 use to_vec_mut::to_vec_mut;
 
-fn explode(data: Data) -> (Data, bool) {
-    panic!("Not implemented");
-    find_explosion_index(data);
-    to_vec_mut(&mut data);
+fn explode(mut data: Data) -> (Data, bool) {
+    // TODO: we need to change [x,y] -> 0
+    if let Some(index_to_explode) = find_explosion_index(&data) {
+        let mut numbers_left_to_right = to_vec_mut(&mut data);
+
+        let right = *numbers_left_to_right.remove(index_to_explode + 1); // remove pairs right element
+        let left = *numbers_left_to_right[index_to_explode];
+
+        *numbers_left_to_right[index_to_explode - 1] += left;
+        *numbers_left_to_right[index_to_explode] = 0;
+        *numbers_left_to_right[index_to_explode + 1] += right;
+
+        (data, true)
+    } else {
+        (data, false)
+    }
 }
 
 #[cfg(test)]
@@ -21,38 +33,38 @@ mod tests {
         serde_json::from_str(raw).unwrap()
     }
 
-    // #[test]
-    // fn one() {
-    //     let res = explode(parse("[[[[[9,8],1],2],3],4]"));
+    #[test]
+    fn one() {
+        let res = explode(parse("[[[[[9,8],1],2],3],4]"));
 
-    //     assert_eq!(parse("[[[[0,9],2],3],4]"), res);
-    // }
+        assert_eq!(res, (parse("[[[[0,9],2],3],4]"), true));
+    }
 
-    // #[test]
-    // fn two() {
-    //     let res = explode(parse("[7,[6,[5,[4,[3,2]]]]]"));
+    #[test]
+    fn two() {
+        let res = explode(parse("[7,[6,[5,[4,[3,2]]]]]"));
 
-    //     assert_eq!(parse("[7,[6,[5,[7,0]]]]"), res);
-    // }
+        assert_eq!(res, (parse("[7,[6,[5,[7,0]]]]"), true));
+    }
 
-    // #[test]
-    // fn three() {
-    //     let res = explode(parse("[[6,[5,[4,[3,2]]]],1]"));
+    #[test]
+    fn three() {
+        let res = explode(parse("[[6,[5,[4,[3,2]]]],1]"));
 
-    //     assert_eq!(parse("[[6,[5,[7,0]]],3]"), res);
-    // }
+        assert_eq!(res, (parse("[[6,[5,[7,0]]],3]"), true));
+    }
 
-    // #[test]
-    // fn four() {
-    //     let res = explode(parse("[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]"));
+    #[test]
+    fn four() {
+        let res = explode(parse("[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]"));
 
-    //     assert_eq!(parse("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]"), res);
-    // }
+        assert_eq!(res, (parse("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]"), true));
+    }
 
-    // #[test]
-    // fn five() {
-    //     let res = explode(parse("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]"));
+    #[test]
+    fn five() {
+        let res = explode(parse("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]"));
 
-    //     assert_eq!(parse("[[3,[2,[8,0]]],[9,[5,[7,0]]]]"), res);
-    // }
+        assert_eq!(res, (parse("[[3,[2,[8,0]]],[9,[5,[7,0]]]]"), true));
+    }
 }
