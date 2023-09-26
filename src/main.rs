49be64def948f0de00333f525967f7f3d359e857
jkaczmarkiewcz <positiveKiddo@gmail.com>
2023-09-26 11:28:28 +0200

    feat(day-8-p1): solution

--- src/main.rs
+++ src/main.rs
@@ -47,4 +47,5 @@ fn main() {
     y2022::d5::answer();
     y2022::d6::answer();
     y2022::d7::answer();
+    y2022::d8::answer();
 }
---
+++ src/y2022/d8/mod.rs
@@ -0,0 +1,91 @@
+use crate::utils::read_lines;
+
+pub fn answer() {
+    println!("Answer to day8: {} {}", a1(), a2());
+}
+
+pub fn a1() -> usize {
+    let map_of_trees = read_lines("src/y2022/d8/input")
+        .map(|row| {
+            row.chars()
+                .map(|c| c.to_digit(10).unwrap())
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
+
+    let mut count = 2 * map_of_trees.len() + 2 * (map_of_trees[0].len() - 2);
+
+    let is_visible = |i: usize, j: usize| {
+        let current_tree_height = map_of_trees[i][j];
+
+        // top
+        if (0..i).all(|k| map_of_trees[k][j] < current_tree_height) {
+            return true;
+        }
+        // bottom
+        if (i + 1..map_of_trees.len()).all(|k| map_of_trees[k][j] < current_tree_height) {
+            return true;
+        }
+        // left
+        if (0..j).all(|k| map_of_trees[i][k] < current_tree_height) {
+            return true;
+        }
+        // right
+        if (j + 1..map_of_trees[0].len()).all(|k| map_of_trees[i][k] < current_tree_height) {
+            return true;
+        }
+
+        false
+    };
+
+    for i in 1..map_of_trees.len() - 1 {
+        for j in 1..map_of_trees[i].len() - 1 {
+            if is_visible(i, j) {
+                count += 1;
+            }
+        }
+    }
+
+    count
+}
+
+// pub fn a2() -> usize {
+//     let map_of_trees = read_lines("src/y2022/d8/input")
+//         .map(|row| {
+//             row.chars()
+//                 .map(|c| c.to_digit(10).unwrap())
+//                 .collect::<Vec<_>>()
+//         })
+//         .collect::<Vec<_>>();
+
+
+//     let is_visible = |i: usize, j: usize| {
+        
+//     };
+
+//     for i in 0..map_of_trees.len() {
+//         for j in 0..map_of_trees[i].len() {
+//         }
+//     }
+
+//     0
+// }
+
+// pub fn a2() -> usize {
+//     0
+// }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn should_solve_first_problem() {
+        assert_eq!(a1(), 1818);
+    }
+
+    #[test]
+    fn should_solve_second_problem() {
+        // assert_eq!(a2(), 0);
+    }
+}
