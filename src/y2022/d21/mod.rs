85a1e2647e1c1a3fde8ec1bbdb6a4e60438ed920
jkaczmarkiewcz <positiveKiddo@gmail.com>
2025-06-22 23:50:14 +0200

    feat(day-21a): first part working

--- src/y2022/d21/mod.rs
+++ src/y2022/d21/mod.rs
@@ -1,16 +1,66 @@
+use std::collections::HashMap;
+
+use serde_json::map;
+
 use crate::utils::read_lines;
 
 pub fn answer() {
     println!("Answer to dayxb: {} {}", a1(), a2());
 }
 
+enum Operation {
+    Value(i64),
+    Multiplication(String, String),
+    Division(String, String),
+    Addition(String, String),
+    Subtration(String, String),
+}
+
+fn get_value_rec(name: &str, map: &HashMap<String, Operation>) -> i64 {
+    map.get(name)
+        .map(|o| match o {
+            Operation::Addition(l, r) => get_value_rec(&l, map) + get_value_rec(&r, map),
+            Operation::Division(l, r) => get_value_rec(&l, map) / get_value_rec(&r, map),
+            Operation::Subtration(l, r) => get_value_rec(&l, map) - get_value_rec(&r, map),
+            Operation::Multiplication(l, r) => get_value_rec(&l, map) * get_value_rec(&r, map),
+            Operation::Value(l) => *l,
+        })
+        .unwrap()
+}
+
 fn a1() -> i64 {
-    let mut list = read_lines("src/y2022/dx/input");
-    0
+    let map = read_lines("src/y2022/d21/input")
+        .map(|l| {
+            let (name, operation_raw) = &l.split_at(6);
+
+            let operation = operation_raw.parse::<i64>().map_or_else(
+                |_| {
+                    let mut iter = operation_raw.split_whitespace();
+
+                    let left_ref = iter.next().unwrap().to_string();
+                    let operator_symbol = iter.next().unwrap();
+                    let right_ref = iter.next().unwrap().to_string();
+
+                    match operator_symbol {
+                        "*" => Operation::Multiplication(left_ref, right_ref),
+                        "/" => Operation::Division(left_ref, right_ref),
+                        "-" => Operation::Subtration(left_ref, right_ref),
+                        "+" => Operation::Addition(left_ref, right_ref),
+                        _ => panic!(),
+                    }
+                },
+                |value| Operation::Value(value),
+            );
+
+            (name[0..4].to_string(), operation)
+        })
+        .collect::<HashMap<_, _>>();
+
+    get_value_rec("root", &map)
 }
 
 fn a2() -> i64 {
-    let mut list = read_lines("src/y2022/dx/input");
+    let mut list = read_lines("src/y2022/d21/input");
     0
 }
 
@@ -21,7 +71,7 @@ mod tests {
 
     #[test]
     fn should_solve_first_problem() {
-        assert_eq!(a1(), 0);
+        assert_eq!(a1(), 56490240862410);
     }
 
     #[test]
