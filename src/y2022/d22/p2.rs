15c16e55fa9ec8891d3ce57636f60a220e3a4846
jkaczmarkiewcz <positiveKiddo@gmail.com>
2025-07-05 09:36:10 +0200

    feat(day-22-2):  abstract repr + thought process

--- src/y2022/d22/p2.rs
+++ src/y2022/d22/p2.rs
@@ -1,8 +1,70 @@
-use crate::utils::read_lines;
+use core::panic;
+
+use crate::utils::read;
+
+#[derive(PartialEq, Eq)]
+enum Tile {
+    Wall,
+    Movable,
+}
 
 pub fn answer() -> i64 {
-    read_lines("src/y2022/dXX/input");
+    let all = read("src/y2022/d22/input");
+    let mut iter = all.split("\n\n");
+    let map_raw = iter
+        .next()
+        .unwrap()
+        .split("\n")
+        .map(|s| s.chars().collect::<Vec<_>>())
+        .collect::<Vec<_>>();
+    let operation_raw = iter.next().unwrap();
 
+    let face_size = if all.len() == 25823 { 50 } else { 4 };
+
+    let mut abstract_representation = Vec::with_capacity(6);
+
+    let mut col = 0;
+    let mut row = 0;
+    loop {
+        if abstract_representation.len() == 6 {
+            break;
+        }
+        if map_raw[row * face_size][col * face_size] != ' ' {
+            abstract_representation.push((
+                (col, row),
+                (0..face_size)
+                    .map(|col_relative| {
+                        (0..face_size)
+                            .map(|row_relative| {
+                                let char = map_raw[row * face_size + row_relative]
+                                    [col * face_size + col_relative];
+
+                                if char == '.' {
+                                    Tile::Movable
+                                } else if char == '#' {
+                                    Tile::Wall
+                                } else {
+                                    panic!();
+                                }
+                            })
+                            .collect::<Vec<_>>()
+                    })
+                    .collect::<Vec<_>>(),
+            ));
+        }
+
+        if map_raw[row * face_size]
+            .get((col + 1) * face_size)
+            .is_some()
+        {
+            col += 1;
+        } else {
+            row += 1;
+            col = 0;
+        }
+    }
+
+    println!("LENGTH: {}", all.len());
     0
 }
 
