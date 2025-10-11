75a191800f6020a269f22d558a48659466956442
jkaczmarkiewcz <positiveKiddo@gmail.com>
2025-10-11 22:13:01 +0200

    feat(day-25-1): answer

--- src/y2022/d25/mod.rs
+++ src/y2022/d25/mod.rs
@@ -2,5 +2,9 @@ pub mod p1;
 pub mod p2;
 
 pub fn answer() -> () {
-    println!("Answer to day25 p1: {}, p2: {}", p1::answer(), p2::answer());
+    println!(
+        "Answer to day25 p1: {:?}, p2: {}",
+        p1::answer(),
+        p2::answer()
+    );
 }
--- src/y2022/d25/p1.rs
+++ src/y2022/d25/p1.rs
@@ -1,9 +1,60 @@
 use crate::utils::read_lines;
 
-pub fn answer() -> i64 {
-    read_lines("src/y2022/dXX/input");
+pub fn answer() -> String {
+    let total_sum = read_lines("src/y2022/d25/input")
+        .map(|line| {
+            line.chars()
+                .rev()
+                .enumerate()
+                .map(|(i, c)| {
+                    (match c {
+                        '=' => -2,
+                        '-' => -1,
+                        '1' => 1,
+                        '2' => 2,
+                        '0' => 0,
 
-    0
+                        n => panic!("Other: {n}"),
+                    }) * 5_i32.pow(i as u32)
+                })
+                .sum::<i32>()
+        })
+        .sum::<i32>();
+
+    return to_snafu(total_sum);
+}
+
+fn to_snafu(mut nr: i32) -> String {
+    let mut result = vec![];
+    while nr != 0 {
+        let mut n = 0;
+
+        'outer: loop {
+            for a in [1 * nr.signum(), 2 * nr.signum()] {
+                let num = a * 5_i32.pow(n);
+                if (nr as f32 / num as f32).round() == 1.0 {
+                    if result.capacity() == 0 {
+                        result = vec!["0"; n as usize + 1];
+                    }
+
+                    result[n as usize] = match a {
+                        1 => "1",
+                        2 => "2",
+                        -2 => "=",
+                        -1 => "-",
+                        _ => panic!(),
+                    };
+                    nr -= num;
+                    break 'outer;
+                }
+            }
+
+            n += 1;
+        }
+    }
+
+    result.reverse();
+    return result.join("");
 }
 
 #[cfg(test)]
@@ -13,6 +64,6 @@ mod tests {
 
     #[test]
     fn should_compute_solution() {
-        assert_eq!(answer(), 0);
+        assert_eq!(answer(), "2=-1=0".to_string());
     }
 }
