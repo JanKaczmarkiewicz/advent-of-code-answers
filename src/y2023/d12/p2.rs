bcaeaf71d169d4718cb7374ad24341b504df7708
jkaczmarkiewcz <positiveKiddo@gmail.com>
2025-12-16 13:38:28 +0100

    feat(2023-12-2): base perf problem, need to optimize

--- src/y2023/d12/p2.rs
+++ src/y2023/d12/p2.rs
@@ -1,9 +1,89 @@
+use itertools::Itertools;
+
 use crate::utils::read_lines;
 
-pub fn answer() -> i64 {
-    read_lines("src/y2023/d12/input");
+fn extract_groups(row: &str) -> Vec<u16> {
+    let mut counter = 0;
 
-    0
+    let mut row_groups = vec![];
+
+    for ch in row.chars() {
+        if ch == '.' {
+            if counter > 0 {
+                row_groups.push(counter);
+                counter = 0;
+            }
+        } else {
+            /* # */
+            counter += 1;
+        }
+    }
+
+    if counter > 0 {
+        row_groups.push(counter);
+    }
+
+    return row_groups;
+}
+
+fn are_vecs_equal<T: PartialEq<T>>(l: &Vec<T>, r: &Vec<T>) -> bool {
+    l.len() == r.len() && l.iter().zip(r.iter()).all(|(l, r)| l == r)
+}
+
+pub fn answer() -> usize {
+    read_lines("src/y2023/d12/input")
+        .enumerate()
+        .map(|(i, line)| {
+            if (i % 10 == 0) {
+                println!("{i}")
+            }
+
+            let (row_map, contiguous_groups_raw) = line.split_once(" ").unwrap();
+
+            let row_map = (0..5).map(|_| row_map).join(",");
+            let contiguous_groups_raw = (0..5).map(|_| contiguous_groups_raw).join(",");
+
+            let contiguous_groups = contiguous_groups_raw
+                .split(",")
+                .map(|num_raw| num_raw.parse::<u16>().unwrap())
+                .collect::<Vec<_>>();
+
+            let mut possibilities = vec!["".to_string()];
+
+            for c in row_map.chars() {
+                if c == '?' {
+                    possibilities = possibilities
+                        .iter()
+                        .flat_map(|el| {
+                            [format!("{}.", el.clone()), format!("{}#", el.clone())].into_iter()
+                        })
+                        .collect()
+                } else {
+                    possibilities.iter_mut().for_each(|el| {
+                        el.push(c);
+                    });
+                }
+
+                possibilities.retain(|el| {
+                    let el_groups = extract_groups(el);
+
+                    if el.len() == row_map.len() {
+                        are_vecs_equal(&el_groups, &contiguous_groups)
+                    } else {
+                        if el_groups.len() == 0 {
+                            return true;
+                        } else {
+                            el_groups.len() <= contiguous_groups.len()
+                                && el_groups[el_groups.len() - 1]
+                                    <= contiguous_groups[el_groups.len() - 1]
+                        }
+                    }
+                })
+            }
+
+            possibilities.len()
+        })
+        .sum()
 }
 
 #[cfg(test)]
@@ -13,6 +93,6 @@ mod tests {
 
     #[test]
     fn should_compute_solution() {
-        assert_eq!(answer(), 0);
+        assert_eq!(answer(), 7163);
     }
 }
