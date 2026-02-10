ca51d7fd260312d5ffc4eaa6579dd4f5a015f1d2
jkaczmarkiewcz <positiveKiddo@gmail.com>
2026-02-10 16:31:22 +0100

    feat(2023-13-2): solution

--- src/y2023/d13/p2.rs
+++ src/y2023/d13/p2.rs
@@ -1,7 +1,9 @@
-use crate::{
-    utils::read_lines,
-    y2023::d13::p1::{get, mirror_vertical},
-};
+use crate::utils::read_lines;
+use itertools::Itertools;
+
+fn get<T>(v: &Vec<T>, i: isize) -> Option<&T> {
+    i.is_positive().then_some(v.get(i as usize)).flatten()
+}
 
 fn find_difference(l: &String, r: &String) -> Option<usize> {
     let mut differance_found = None;
@@ -30,19 +32,20 @@ pub fn mirror_horizontal(board: &Vec<String>) -> Option<usize> {
             } else {
                 false
             };
-            if is_smudge {
-                was_smudge = true
-            };
-
-            up == down || is_smudge
+            if is_smudge && !was_smudge {
+                was_smudge = true;
+                true
+            } else {
+                up == down
+            }
         } {
             dir_mod += 1;
         }
 
         if dir_mod != 0
             && was_smudge
-            && (get(board, row_i as isize - dir_mod) == None
-                || get(board, row_i as isize + dir_mod + 1) == None)
+            && (get(board, row_i as isize - dir_mod).is_none()
+                || get(board, row_i as isize + dir_mod + 1).is_none())
         {
             return Some(row_i + 1);
         }
@@ -51,29 +54,43 @@ pub fn mirror_horizontal(board: &Vec<String>) -> Option<usize> {
     None
 }
 
+pub fn mirror_vertical(board: &Vec<String>) -> Option<usize> {
+    let board_transposed = (0..board[0].len())
+        .map(|i| {
+            board
+                .iter()
+                .map(|row| row.chars().nth(i as usize).unwrap())
+                .join("")
+        })
+        .collect::<Vec<_>>();
+
+    mirror_horizontal(&board_transposed)
+}
+
+fn find_mirror(board: &Vec<String>) -> usize {
+    if let Some(mirror) = mirror_horizontal(&board) {
+        (mirror) * 100
+    } else if let Some(mirror) = mirror_vertical(&board) {
+        mirror
+    } else {
+        panic!()
+    }
+}
+
 pub fn answer() -> usize {
     let mut curr_board = vec![];
     let mut result = 0;
 
-    for line in read_lines("src/y2023/d13/input_example") {
+    for line in read_lines("src/y2023/d13/input") {
         if line.replace(" ", "").is_empty() {
-            if let Some(mirror) = mirror_horizontal(&curr_board) {
-                result += (mirror) * 100;
-            } else if let Some(mirror) = mirror_vertical(&curr_board) {
-                result += mirror;
-            }
-
+            result += find_mirror(&curr_board);
             curr_board.clear();
             continue;
         }
         curr_board.push(line);
     }
 
-    if let Some(mirror) = mirror_horizontal(&curr_board) {
-        result += mirror * 100;
-    } else if let Some(mirror) = mirror_vertical(&curr_board) {
-        result += mirror;
-    }
+    result += find_mirror(&curr_board);
 
     result
 }
@@ -85,6 +102,6 @@ mod tests {
 
     #[test]
     fn should_compute_solution() {
-        assert_eq!(answer(), 0);
+        assert_eq!(answer(), 36474);
     }
 }
