4eae2e28c7259a6a28dd5a1207be716814199aae
jkaczmarkiewcz <positiveKiddo@gmail.com>
2026-02-13 20:41:30 +0100

    feat(2023-22-1): solution

--- src/y2023/d22/p1.rs
+++ src/y2023/d22/p1.rs
@@ -16,6 +16,13 @@ fn space((start, end): ((i32, i32, i32), (i32, i32, i32))) -> HashSet<(i32, i32,
     }
 }
 
+fn check_sum(bricks: &Vec<((i32, i32, i32), (i32, i32, i32))>) -> i32 {
+    bricks
+        .iter()
+        .map(|((s1, s2, s3), (e1, e2, e3))| s1 + s2 + s3 + e1 + e2 + e3)
+        .sum::<i32>()
+}
+
 pub fn answer() -> usize {
     let mut bricks = read_lines("src/y2023/d22/input")
         .map(|str| {
@@ -37,62 +44,69 @@ pub fn answer() -> usize {
         })
         .collect_vec();
 
-    for i in 0..bricks.len() {
-        if bricks[i].0 .2 == 1 {
-            continue;
+    let mut prev_check_sum = 0;
+
+    loop {
+        let current_check_sum = check_sum(&bricks);
+        if prev_check_sum == current_check_sum {
+            break;
+        } else {
+            prev_check_sum = current_check_sum;
         }
 
-        bricks[i].0 .2 -= 1;
-        bricks[i].1 .2 -= 1;
-
-        for j in 0..bricks.len() {
-            if j == i {
+        // TODO
+        for i in 0..bricks.len() {
+            if bricks[i].0 .2 == 1 {
                 continue;
             }
-            if space(bricks[i]).difference(&space(bricks[j])).count() != 0 {
-                bricks[i].0 .2 += 1;
-                bricks[i].1 .2 += 1;
-                break;
+
+            bricks[i].0 .2 -= 1;
+            bricks[i].1 .2 -= 1;
+
+            for j in 0..bricks.len() {
+                if j == i {
+                    continue;
+                }
+                if space(bricks[i]).intersection(&space(bricks[j])).count() != 0 {
+                    bricks[i].0 .2 += 1;
+                    bricks[i].1 .2 += 1;
+                    break;
+                }
             }
         }
     }
 
     (0..bricks.len())
         .filter(|disabled| {
-            // disable loop
             let disabled_iter = (0..bricks.len()).filter(|i| i != disabled);
 
             // check if some brick can be lovered given brick removal
-            let r = disabled_iter.clone().all(|i| {
+            disabled_iter.clone().all(|i| {
                 if bricks[i].0 .2 == 1 {
+                    // stable on ground
                     return true;
                 }
 
-                bricks[i].0 .2 -= 1;
-                bricks[i].1 .2 -= 1;
+                let mut current_brick = bricks[i].clone();
+                current_brick.0 .2 -= 1;
+                current_brick.1 .2 -= 1;
+
+                let current_brick_space = space(current_brick);
 
                 for j in disabled_iter.clone() {
                     if j == i {
                         continue;
                     }
 
-                    if space(bricks[i]).difference(&space(bricks[j])).count() != 0 {
-                        // collision exist
-                        bricks[i].0 .2 += 1;
-                        bricks[i].1 .2 += 1;
-
+                    if current_brick_space.intersection(&space(bricks[j])).count() != 0 {
+                        // collision exist, brick will still be stable
                         return true;
                     }
                 }
 
                 // no collision, brick can be moved down
-                bricks[i].0 .2 += 1;
-                bricks[i].1 .2 += 1;
-
                 return false;
-            });
-            println!("{disabled} {r}");
-            r
+            })
         })
         .count()
 }
