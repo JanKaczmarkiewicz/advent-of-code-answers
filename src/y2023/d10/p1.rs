d7667165ecad7c3203013858d4d2643d148eb376
jkaczmarkiewcz <positiveKiddo@gmail.com>
2026-02-15 16:51:21 +0100

    feat(2023-10-2): solution

--- src/y2023/d10/p1.rs
+++ src/y2023/d10/p1.rs
@@ -4,7 +4,7 @@ use itertools::Itertools;
 
 use crate::utils::read_lines;
 
-fn get_cell(map: &Vec<Vec<char>>, (row, col): (i32, i32)) -> Option<&char> {
+pub fn get_cell(map: &Vec<Vec<char>>, (row, col): (i32, i32)) -> Option<&char> {
     if row >= 0 && col >= 0 {
         map.get(row as usize).and_then(|r| r.get(col as usize))
     } else {
@@ -23,19 +23,15 @@ fn find_start(map: &Vec<Vec<char>>) -> (i32, i32) {
     panic!()
 }
 
-pub fn answer() -> f64 {
-    let map = read_lines("src/y2023/d10/input")
-        .map(|l| l.chars().collect_vec())
-        .collect_vec();
-
-    let start = find_start(&map);
+pub fn find_loop(map: &Vec<Vec<char>>) -> HashSet<(i32, i32)> {
+    let start = find_start(map);
 
     let mut paths = vec![(HashSet::new(), start)];
 
     while let Some((mut history, current)) = paths.pop() {
         if history.contains(&current) {
             if current == start && history.len() > 2 {
-                return history.len() as f64 / 2.0;
+                return history;
             } else {
                 continue;
             }
@@ -43,13 +39,13 @@ pub fn answer() -> f64 {
 
         history.insert(current);
 
-        let c = get_cell(&map, current).unwrap();
+        let c = get_cell(map, current).unwrap();
 
         //up
         if let '|' | 'L' | 'J' | 'S' = c {
             let next = (current.0 - 1, current.1);
 
-            if let Some('|' | 'F' | '7' | 'S') = get_cell(&map, next) {
+            if let Some('|' | 'F' | '7' | 'S') = get_cell(map, next) {
                 paths.push((history.clone(), next))
             }
         }
@@ -58,7 +54,7 @@ pub fn answer() -> f64 {
         if let '|' | 'F' | '7' | 'S' = c {
             let next = (current.0 + 1, current.1);
 
-            if let Some('|' | 'L' | 'J' | 'S') = get_cell(&map, next) {
+            if let Some('|' | 'L' | 'J' | 'S') = get_cell(map, next) {
                 paths.push((history.clone(), next))
             }
         }
@@ -67,8 +63,7 @@ pub fn answer() -> f64 {
         if let '-' | 'L' | 'F' | 'S' = c {
             let next = (current.0, current.1 + 1);
 
-            println!("{:?}", get_cell(&map, next));
-            if let Some('-' | 'J' | '7' | 'S') = get_cell(&map, next) {
+            if let Some('-' | 'J' | '7' | 'S') = get_cell(map, next) {
                 paths.push((history.clone(), next))
             }
         }
@@ -77,13 +72,21 @@ pub fn answer() -> f64 {
         if let '-' | 'J' | '7' | 'S' = c {
             let next = (current.0, current.1 - 1);
 
-            if let Some('-' | 'L' | 'F' | 'S') = get_cell(&map, next) {
+            if let Some('-' | 'L' | 'F' | 'S') = get_cell(map, next) {
                 paths.push((history.clone(), next))
             }
         }
     }
 
-    0.0
+    panic!()
+}
+
+pub fn answer() -> f64 {
+    let map = read_lines("src/y2023/d10/input")
+        .map(|l| l.chars().collect_vec())
+        .collect_vec();
+
+    find_loop(&map).len() as f64 / 2.0
 }
 
 #[cfg(test)]
--- src/y2023/d10/p2.rs
+++ src/y2023/d10/p2.rs
@@ -1,5 +1,73 @@
-pub fn answer() -> i64 {
-    0
+use std::collections::HashSet;
+
+use itertools::Itertools as _;
+
+use crate::{
+    utils::read_lines,
+    y2023::d10::p1::{find_loop, get_cell},
+};
+
+pub fn answer() -> usize {
+    let map = read_lines("src/y2023/d10/input_example")
+        .map(|l| l.chars().collect_vec())
+        .collect_vec();
+
+    let found_loop = find_loop(&map);
+
+    let (min_col, max_col) = found_loop
+        .iter()
+        .map(|(_, col)| *col)
+        .minmax()
+        .into_option()
+        .map(|(min, max)| (min - 1, max + 1))
+        .unwrap();
+
+    let (min_row, max_row) = found_loop
+        .iter()
+        .map(|(row, _)| *row)
+        .minmax()
+        .into_option()
+        .map(|(min, max)| (min - 1, max + 1))
+        .unwrap();
+
+    println!("{:?}", ((min_col, max_col), (min_row, max_row)));
+
+    let mut visited = HashSet::new();
+    let mut to_visit = vec![(min_row, min_col)];
+
+    while let Some(current) = to_visit.pop() {
+        visited.insert(current);
+
+        to_visit.extend(
+            [
+                (current.0 - 1, current.1),
+                (current.0 + 1, current.1),
+                (current.0, current.1 + 1),
+                (current.0, current.1 - 1),
+            ]
+            .iter()
+            .filter(|(row, col)| {
+                (min_col..=max_col).contains(col)
+                    && (min_row..=max_row).contains(row)
+                    && !found_loop.contains(&(*row, *col))
+                    && !visited.contains(&(*row, *col))
+            }),
+        );
+    }
+
+    let tiles = (min_col..=max_col)
+        .flat_map(|col| (min_row..=max_row).map(move |row| (row, col)))
+        .collect::<HashSet<_>>();
+
+    let to_exclude = visited
+        .union(&found_loop)
+        .map(|a| *a)
+        .collect::<HashSet<_>>();
+
+    tiles
+        .difference(&to_exclude)
+        .filter_map(|cord| get_cell(&map, *cord).map(|cell| *cell == '.').map(|_| cord))
+        .count()
 }
 
 #[cfg(test)]
@@ -9,6 +77,6 @@ mod tests {
 
     #[test]
     fn should_compute_solution() {
-        assert_eq!(answer(), 0);
+        assert_eq!(answer(), 10);
     }
 }
